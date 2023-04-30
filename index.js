91b16f83d6b7d1ec903fac85e29fdec7aca3c854
evlev777 <levkovich.evgeniy.98@gmail.com>
2023-04-30 20:32:49 +0300

    feat: add function for change language

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import virtualKeyBoardHTML from './assets/virtual-keyboard.js'
+import langRu from './assets/langRu.js';
+import langEn from './assets/langEn.js'
+import changeKeyboard from './assets/changeKeyBoard.js'
 
 async function startApp(){
 
+    localStorage.setItem('lang', localStorage.getItem('lang') || 'en');
+    let layout = localStorage.getItem('lang') === 'en' ? langEn : langRu;
 
     const virtualKeyboard = document.querySelector('.virtual__keyboard');
     virtualKeyboard.insertAdjacentHTML('afterbegin',virtualKeyBoardHTML);
 
+    let mode = 'normal';
+    const row = document.querySelector('.row');
+    const keyboard = document.querySelector('.keyboard');
+
+    changeKeyboard(mode, layout, row);
     const textarea = document.querySelector('textarea');
     textarea.focus()
 }
